184171c92f0fea07d0c523d23ed9413f568b34ca
Gabriel Padilha <gabrielpadilha@ciandt.com>
2021-09-09 06:18:45 -0300

    projeto atualizado para o site lenain

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -1,23 +1,43 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage{
 
-    public void preencherEmail(String informacao){
-        preencherCampo("Email", informacao);
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    public void preencherSenha(String informacao){
-        preencherCampo("Senha", informacao);
+    @FindBy(name = "login")
+    public WebElement login;
+
+    @FindBy(name = "pass")
+    public WebElement password;
+
+    @FindBy(xpath = "//*[@type='submit']")
+    public WebElement btnLogOn;
+
+    public void preencherEmail(String email){
+        login.sendKeys(email);
+    }
+
+    public void preencherSenha(String senha){
+        password.sendKeys(senha);
     }
 
     public void entrar(){
-        clicarBotao("Entrar");
+        clicarBotao("Confirmar");
     }
 
-    public String validarMensagemSucesso(){
-        return obterTexto(By.xpath("//*[@class='alert alert-success']"));
+    public String validarTexto(String texto){
+        return obterTexto(By.xpath("//*[text()='" + texto + "']"));
     }
 
     public String validarMensagemErro(){
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -7,11 +7,11 @@ import static manager.DriverFactory.getDriver;
 import static manager.DriverFactory.killDriver;
 
 public class LoginTest {
-    LoginPage loginPage = new LoginPage();
+    LoginPage loginPage = new LoginPage(getDriver());
 
     @Before
     public void acessarTelaInicial(){
-        getDriver().get("https://seubarriga.wcaquino.me/login");
+        getDriver().get("https://www.lenain.com.br/restrito/index.php?pagina=login");
     }
 
     @After
@@ -21,25 +21,10 @@ public class LoginTest {
 
     @Test
     public void efetuarLoginComSucesso(){
-        loginPage.preencherEmail("padilha.ga@gmail.com");
-        loginPage.preencherSenha("gABRIEL");
+        loginPage.preencherEmail("usuarioteste@lenain.com.br");
+        loginPage.preencherSenha("123!@#as");
         loginPage.entrar();
-        Assert.assertEquals("Bem vindo, Gabriel!", loginPage.validarMensagemSucesso());
+        Assert.assertEquals("Online", loginPage.validarTexto(" Online"));
     }
-
-    @Test
-    public void efetuarLoginComEmailInvalido(){
-        loginPage.preencherEmail("padilha123.ga@gmail.com");
-        loginPage.preencherSenha("gABRIEL");
-        loginPage.entrar();
-        Assert.assertEquals("Problemas com o login do usuário", loginPage.validarMensagemErro());
-    }
-
-    @Test
-    public void efetuarLoginComSenhaInvalida(){
-        loginPage.preencherEmail("padilha.ga@gmail.com");
-        loginPage.preencherSenha("gABRIEL123");
-        loginPage.entrar();
-        Assert.assertEquals("Problemas com o login do usuário", loginPage.validarMensagemErro());
-    }
+    
 }
